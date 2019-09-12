7e20878656f1c722ac2164ffd0b786a0b555935b
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-09-12 18:40:07 +0200

    list of types added

--- app/models/cultural_good.rb
+++ app/models/cultural_good.rb
@@ -1,5 +1,6 @@
 class CulturalGood < ApplicationRecord
   validates :title, :type, :thematic, :creator_id, presence: true
+  validates :type, inclusion: { in: ["Exhibition", "Cinema", "Book", "Comic", "Stand-up", "Theatre", "Opera", "Dancing", "Concert", "TV Show", "Documentary"] }
   belongs_to :creator
   has_many :memories
 end
