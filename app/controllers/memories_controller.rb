382097a1ee29fcdf0e56d1a2996f45d9b71fcad7
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-09-16 10:44:50 +0200

    form created

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,14 +4,17 @@ class MemoriesController < ApplicationController
 	def home
 		@memories = Memory.all
 		@memory = Memory.new
+		cultural_good = @memory.build_cultural_good
+		cultural_good.build_creator
+		@memory.build_venue
 	end
 
 	def show
 	end
 
-	def new
-		@memory = Memory.new
-	end
+	# def new
+	# 	@memory = Memory.new
+	# end
 
 	def create
 		@memory = Memory.new(memories_params)
@@ -19,7 +22,9 @@ class MemoriesController < ApplicationController
 		if @memory.save
 		  redirect_to root_path
 		else
-		  render :new
+		  puts "-------------"
+		  puts @memory.errors.to_h
+		  render :home
 		end
 	end
 
@@ -39,6 +44,6 @@ class MemoriesController < ApplicationController
 	end
 
 	def memories_params
-	  params.require(:memories).permit(:date, :rating, :review, cultural_goods_attributes: [:title, :type, :thematic, creators_attributes: [:name]])
+	  params.require(:memory).permit(:date, :rating, :review, :venue_id, venue_attributes: [:name, :id], cultural_good_attributes: [:title, :cultural_type, :thematic, creator_attributes: [:name]])
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_153903) do
+ActiveRecord::Schema.define(version: 2019_09_13_142210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_153903) do
 
   create_table "cultural_goods", force: :cascade do |t|
     t.string "title"
-    t.string "type"
+    t.string "cultural_type"
     t.string "thematic"
     t.bigint "creator_id"
     t.datetime "created_at", null: false
