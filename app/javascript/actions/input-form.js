7c1461e5c49fca8ccc97a9fd4e35471fef212ac0
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-09-17 12:15:42 +0200

    dropdown movies adapted to selection of cinema category

--- app/javascript/actions/input-form.js
+++ app/javascript/actions/input-form.js
@@ -1,17 +1,17 @@
-const suggestMovies = () => {
+const suggestCulturalGoods = () => {
 
-	function findMovies() {
-	    const url = `https://api.themoviedb.org/3/search/movie?api_key=263e31d1ad0c4defa8822787e614e716&language=en-US&query=${this.value}&page=1&include_adult=false`;
-	    if (this.value) {
+	function findMovies(event) {
+	    const url = `https://api.themoviedb.org/3/search/movie?api_key=263e31d1ad0c4defa8822787e614e716&language=en-US&query=${event.value}&page=1&include_adult=false`;
+	    if (event.value) {
 		    fetch(url)
 		      .then(blob => blob.json())
-		      .then(data => displayMovies(data));
+		      .then(data => displayCulturalGoods(data));
 	    } else {
 	    	suggestions.innerHTML = "";
 	    }
 	}
 
-	function displayMovies(data) {
+	function displayCulturalGoods(data) {
 	  	const moviesArray = []
 		  	data.results.forEach(result => moviesArray.push(result.original_title))
 		  	const html = moviesArray.map(movie => {
@@ -23,29 +23,29 @@ const suggestMovies = () => {
 		  	}).join('');
 		  	suggestions.innerHTML = html;
 
-	function selectMovie() {
-		const movies = document.querySelectorAll(".movie");
-		if (movies) {
-			movies.forEach(movie => {
-				movie.addEventListener("click", e => {
-				console.log("we're in")
-				titleInput.value = e.currentTarget.innerText;
-				suggestions.innerHTML = "";
+		function selectCulturalGood() {
+			const movies = document.querySelectorAll(".movie");
+			if (movies) {
+				movies.forEach(movie => {
+					movie.addEventListener("click", e => {
+					titleInput.value = e.currentTarget.innerText;
+					suggestions.innerHTML = "";
+					});
 				});
-			});
+			}
 		}
-	}
-	selectMovie();
-
-
+		selectCulturalGood();
 	}
 
+	const typeInput = document.querySelector("#memory_cultural_good_attributes_cultural_type");
 	const titleInput = document.querySelector("#memory_cultural_good_attributes_title");
 	const suggestions = document.querySelector('.suggestions');
 
 	if (titleInput) {
-	    titleInput.addEventListener("keyup", findMovies);
+		titleInput.addEventListener("keyup", function() {
+			if (typeInput.value == "Cinema") { findMovies(this) };
+		})	 
 	}
 }
 
-export {suggestMovies};
+export {suggestCulturalGoods};
