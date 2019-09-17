129ed8afb6100eafddce6d4b678b5b7aa118eccd
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-09-17 11:22:15 +0200

    select a movie with scrolldown from API

--- app/javascript/actions/input-form.js
+++ app/javascript/actions/input-form.js
@@ -1,31 +1,50 @@
 const suggestMovies = () => {
 
-	const findMovies = (wordToMatch) => {
-		const url = `https://api.themoviedb.org/3/search/movie?api_key=263e31d1ad0c4defa8822787e614e716&language=en-US&query=${wordToMatch}&page=1&include_adult=false`
-		const movies = [];
-		fetch(url)
-		  .then(blob => blob.json())
-		  .then(data => data.results.forEach(result => movies.push(result.original_title)));
-		return movies;
+	function findMovies() {
+	    const url = `https://api.themoviedb.org/3/search/movie?api_key=263e31d1ad0c4defa8822787e614e716&language=en-US&query=${this.value}&page=1&include_adult=false`;
+	    if (this.value) {
+		    fetch(url)
+		      .then(blob => blob.json())
+		      .then(data => displayMovies(data));
+	    } else {
+	    	suggestions.innerHTML = "";
+	    }
 	}
 
-	function displayMovies() {
-	  const moviesArray = findMovies(this.value);
-	  const html = moviesArray.map(movie => {
-	    return `
-	      <li>
-	        <span class="movie">${movieName}</span>
-	      </li>
-	    `;
-	  });
-	  suggestions.innerHTML = html;
+	function displayMovies(data) {
+	  	const moviesArray = []
+		  	data.results.forEach(result => moviesArray.push(result.original_title))
+		  	const html = moviesArray.map(movie => {
+			    return `
+			      <li class="movie">
+			        <span>${movie}</span>
+			      </li>
+			    `;
+		  	}).join('');
+		  	suggestions.innerHTML = html;
+
+	function selectMovie() {
+		const movies = document.querySelectorAll(".movie");
+		if (movies) {
+			movies.forEach(movie => {
+				movie.addEventListener("click", e => {
+				console.log("we're in")
+				titleInput.value = e.currentTarget.innerText;
+				suggestions.innerHTML = "";
+				});
+			});
+		}
 	}
-	
+	selectMovie();
+
+
+	}
+
 	const titleInput = document.querySelector("#memory_cultural_good_attributes_title");
 	const suggestions = document.querySelector('.suggestions');
 
 	if (titleInput) {
-		titleInput.addEventListener("keyup", displayMovies);
+	    titleInput.addEventListener("keyup", findMovies);
 	}
 }
 
